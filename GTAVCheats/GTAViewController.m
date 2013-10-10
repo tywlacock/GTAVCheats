8c23c475390d3951da6e49146c513a30252180a3
tylacock <tylacock@gmail.com>
2013-10-09 22:09:04 -0400

    added inital screen background

--- GTAVCheats/GTAViewController.m
+++ GTAVCheats/GTAViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     self.banner.delegate = self;
     UIImage *backgroundImage = [[UIImage alloc]init];
-    backgroundImage = [UIImage imageNamed:@"gta5.jpg"];
+    backgroundImage = [UIImage imageNamed:@"gta5small.jpg"];
     [self.view setBackgroundColor:[UIColor colorWithPatternImage:backgroundImage]];
 }
 
@@ -27,6 +27,23 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+// -------------------------------------------------------------------------- //
+//                           iAd Banner Methods                               //
+// -------------------------------------------------------------------------- //
+- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave
+{
+    return YES;
+}
+
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner
+{
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
 }
 
 @end
