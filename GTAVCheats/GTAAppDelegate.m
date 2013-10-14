82ae56c66257f37b4624a07e59b5a0ea58d23700
tylacock <tylacock@gmail.com>
2013-10-13 22:34:11 -0400

    Removed Speak button and added it to the code UILabel instead

--- GTAVCheats/GTAAppDelegate.m
+++ GTAVCheats/GTAAppDelegate.m
@@ -23,11 +23,9 @@
     
     // Title Font Customization
     [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
-                                                           [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], UITextAttributeTextColor,
-                                                           [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8],UITextAttributeTextShadowColor,
-                                                           [NSValue valueWithUIOffset:UIOffsetMake(0, 1)],
-                                                           UITextAttributeTextShadowOffset,
-                                                           [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:21.0], UITextAttributeFont, nil]];
+                                                           [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                                           [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:21.0], NSFontAttributeName, nil]];
+    
     
     // Back and Done Button color change
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
